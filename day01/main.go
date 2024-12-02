4b717706846293535db1222850ec3664557252e3
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-12-02 00:19:10 +0000

    feat: day01: bootstrapped input.txt and part1(), part2() fns

--- day01/main.go
+++ day01/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+)
+
+// Embed input.txt within the binary as a string
+//go:embed input.txt
+var input_text string
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	println("Hello")
+	// Parse command line inputs
+	var part int
+	flag.IntVar(&part, "part", 1, "part '1' or '2'")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	// Run the selected part and print the answer
+	var ans int
+	if part == 1 {
+		ans = part1(input_text)
+	} else {
+		ans = part2(input_text)
+	}
+	fmt.Println("Output:", ans)
+}
+
+func part1(input_text string) int {
+	return 1
+}
+
+func part2(input_text string) int {
+	return 2
 }
